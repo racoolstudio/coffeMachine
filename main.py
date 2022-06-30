82e1a8fc1db23af48f892ec19dff3fe4ce7e71c7
racoolstudio <75172422+racoolstudio@users.noreply.github.com>
2022-06-29 22:00:28 -0230

    second

--- main.py
+++ main.py
@@ -1,79 +1,44 @@
-# This is a sample Python script.
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color('azure4')
+# timmy.fd(100)
+# screen = Screen()
+# screen.exitonclick()
+# import prettytable
+# table = prettytable.PrettyTable()
+# table.add_column('Pokemon Name', ['Pikachu', 'Squirtle', 'Charmander'])
+# table.add_column('Type', ['Electric', 'Water', 'Fire'])
+# table.align = 'l'
+# print(table)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-from data import *
-
-Q = 0.25
-D = 0.1
-N = 0.05
-P = 0.01
-
-
-def main():
-    return coffee_machine()
-
-
-def coffee_machine():
-    switch = True
-    water = resources['water']
-    milk = resources['milk']
-    coffee = resources['coffee']
-    money = 0
-    while switch:
-        action = input(' What would you like ? (espresso/latte/cappuccino): ').lower()
-        while action == 'report':
-            print(f'Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: ${money}')
-            action = input(' What would you like ? (espresso/latte/cappuccino): ').lower()
-        if action == 'off':
-            switch = False
-            return switch
-        if (action == 'latte' or action == 'cappuccino') and (milk < MENU["latte"]["ingredients"]["milk"] or milk < MENU["cappuccino"]["ingredients"]["milk"]):
-            print('No enough milk 🥛 sorry !')
-            return
-        if water < MENU["latte"]["ingredients"]["water"]  or water < MENU["cappuccino"]["ingredients"]["water"] or water < MENU["espresso"]["ingredients"]["water"]:
-            print('No enough water 💧 sorry !')
-            return
-        if coffee < MENU["espresso"]["ingredients"]["coffee"] or coffee < MENU["latte"]["ingredients"]["coffee"] or coffee < MENU["cappuccino"]["ingredients"]["coffee"]:
-            print('NO enough coffee ☕️ sorry !')
-            return
-        print('Please insert coins.')
-        quarters = int(input('how many quarters?: '))
-        dimes = int(input('how many dimes?: '))
-        nickles = int(input('how many nickles?: '))
-        pennies = int(input('how many pennies?: '))
-        payment = (quarters * Q) + (dimes * D) + (nickles * N) + (pennies * P)
-        if action == 'espresso':
-            e = MENU["espresso"]["ingredients"]
-            water -= e["water"]
-            coffee -= e["coffee"]
-            money += MENU["espresso"]['cost']
-        elif action == 'latte':
-            l = MENU["latte"]["ingredients"]
-            water -= l["water"]
-            milk -= l["milk"]
-            coffee -= l["coffee"]
-            money += MENU["latte"]['cost']
-        elif action == 'cappuccino':
-            c = MENU["cappuccino"]["ingredients"]
-            water -= c["water"]
-            milk -= c["milk"]
-            coffee -= c["coffee"]
-            money += MENU["cappuccino"]['cost']
+import coffee_maker
+import money_machine
+import menu
+machine1 = coffee_maker.CoffeeMaker()
+machine1r = menu.Menu()
+money = money_machine.MoneyMachine()
+switch = 'on'
+while switch == 'on':
+    user = input(f' What would you like ? {machine1r.get_items()} :').lower()
+    if user == 'report':
+        machine1.report()
+        money.report()
+    elif user == 'off':
+        switch = user
+    else:
+        order = machine1r.find_drink(user)
+        if order:
+            if machine1.is_resource_sufficient(order):
+                money.make_payment(order.cost)
+                machine1.make_coffee(order)
+            else:
+                print('Sorry no more coffee for today !')
+                switch = 'off'
         else:
-            print('wrong input try again !')
-
-        balance = payment - money
-        if payment >= money:
-            print(f'Here is ${round(balance, 2)} in change 💵\nHere is your {action} ☕. Enjoy !')
-        else:
-            print('Sorry you don\'t have enough money 😩. Money refunded ♻️')
-
-main()
+            print(f'Enter one of the following :\nreport \noff\n{machine1r.get_items()} ')
 
 
-# Press the green button in the gutter to run the script.
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
